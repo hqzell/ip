f6a914999d30136bf8bcd3ea3ee253168fc21929
hqzell <167740146+hqzell@users.noreply.github.com>
2026-01-22 22:39:48 +0800

    Added user response

--- src/main/java/Cha.java
+++ src/main/java/Cha.java
@@ -1,13 +1,30 @@
-public class Cha {
+import java.util.Scanner;
+
+public class Cha {    
+
     public static void main(String[] args) {
         String logo =         
-          "  _____ _   _     _      \n"
-        + " / ____| | | |   / \\    ~~~\n"
-        + "| |    | |_| |  / _ \\    \n"
-        + "| |    |  _  | / ___ \\   \n"
-        + "| |____| | | |/_/   \ \\  \n"
-        + " \\____|_| |_|/_/    \ \\  \n"
+          "  _____ _   _     _        ~~      \n"
+        + " / ____| | | |   / \\   ___~_~~____\n"
+        + "| |    | |_| |  / _ \\  |         | \n"
+        + "| |    |  _  | / ___ \\ |_________|\n"
+        + "| |____| | | |/ /   \\ \\ \\        /\n"
+        + " \\_____|_| |_|_/     \\_\\ \\______/\n"
         ;
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm\n" + logo
+            + "\nWhat Cha can I get for you?\n" 
+         + "____________________________________________________________\n");
+
+        String user = scanner.nextLine();
+        while (!user.equals("bye")) {
+            System.out.println(user +
+                "\n____________________________________________________________\n");
+            user = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println("CHA CHA! See you again soon!\n" 
+            + "____________________________________________________________");
     }
 }
