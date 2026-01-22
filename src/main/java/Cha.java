15b37740a4dc9340d0d5979ee7719c61ad0baf3b
hqzell <167740146+hqzell@users.noreply.github.com>
2026-01-22 21:50:19 +0800

    Level-0

---
+++ src/main/java/Cha.java
@@ -0,0 +1,13 @@
+public class Cha {
+    public static void main(String[] args) {
+        String logo =         
+          "  _____ _   _     _      \n"
+        + " / ____| | | |   / \\    ~~~\n"
+        + "| |    | |_| |  / _ \\    \n"
+        + "| |    |  _  | / ___ \\   \n"
+        + "| |____| | | |/_/   \ \\  \n"
+        + " \\____|_| |_|/_/    \ \\  \n"
+        ;
+        System.out.println("Hello from\n" + logo);
+    }
+}
